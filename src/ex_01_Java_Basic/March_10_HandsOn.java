a5fd60f402b8b8a107011b1f70f31b4658f11ea6
[VidyaSree-Gotla] <[gotlavidyasree@gmail.com]>
2025-03-10 22:24:46 +0530

    March 10 commit

---
+++ src/ex_01_Java_Basic/March_10_HandsOn.java
@@ -0,0 +1,19 @@
+package ex_01_Java_Basic;
+
+public class March_10_HandsOn {
+    public static void main(String[] args) {
+        //-->Single line Comments
+        /* -------
+        -------------
+          Multi Line Comments*/
+        System.out.print("Prints with out next line i.e.m Side by Side");
+        System.out.println("Prints Line by Line");
+        System.out.println("Addition of 2 and 2 is: " + (2+2));
+        System.out.println("Substrction of 2 and 2 is: " + (2-2));
+        System.out.println("Multiplication of 2 and 2 is: " + (2*2));
+        System.out.println("Division of 2 and 2 is: " + (2/2));
+        System.out.println("Modulus of 2 and 2 is: " + (2%2));
+
+    }
+
+}
