d67a4e1fc190f93207a8a104bd3f1ccce38ef296
[VidyaSree-Gotla] <[gotlavidyasree@gmail.com]>
2025-03-13 07:02:13 +0530

    March 13 commit

---
+++ src/ex_03_Java_Basic/Lab005_Task.java
@@ -0,0 +1,9 @@
+package ex_03_Java_Basic;
+
+public class Lab005_Task {
+    public static void main(String[] args) {
+        int a=10,b=10,c=10;
+        double x= Math.cbrt(Math.pow(a,2)+Math.pow(b,2)-Math.abs(c));
+        System.out.println(x);
+    }
+}
